f9019439be8e8f78635833e8211109745370a3dd
Ramiz3564 <ramiz3564@wrdsb.ca>
2020-09-15 15:10:34 +0000

    I finished practice question 3

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+ import java.util.Scanner;
+
 /**
- *
- * @author 
+ * This program puts numbers in order
+ * @author Zach Ramirez
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+  //creates a scanner for user input
+  Scanner input = new Scanner(System.in); 
+
+  //prompt user for their numbers 
+  System.out.println("Please enter an integer");
+  int integer = input.nextInt(); 
+  System.out.println("please enter your second integer");
+  int secondInteger = input.nextInt(); 
+
+  //determine how the numbers will ascend in order
+  if(integer >= secondInteger){
+    System.out.println(" your numbers in ascending order are " + secondInteger + "," +integer);
+  }else{ 
+    System.out.println(" your numbers in ascending order are " + integer+ "," + secondInteger);
+  }
+    
+
     
   }
 }
